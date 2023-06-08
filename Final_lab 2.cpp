ab29faac1ea7218077e98c3f2f87904d4a636c77
Mahamudul Hasan Mithun <mhmmithun1@gmail.com>
2023-06-08 17:59:57 +0600

    Final_lab 2

---
+++ Final_lab 2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,first=0,second=1;
+     scanf("%d",&a);
+     printf("%d, %d,",first,second);
+     for (int i=2;i<a;i++)
+     {
+        int b=first+second;
+        printf(" %d,",b);
+
+        first=second;
+        second=b;
+
+     }
+
+    return 0;
+}
